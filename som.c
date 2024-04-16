dd80c25e0e969e0672d5621375793b1d0e590e68
lucasalves561 <lucasalves55682@gmail.com>
2024-04-16 18:30:13 -0300

    first commit

---
+++ som.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int n1, n2, soma;
+  printf("Digite um numero: ");
+   scanf("%i", &n1);
+
+   printf("Digite outro numero: ");
+   scanf("%i", &n2);
+
+    soma = (n1 + n2);
+      printf ("a soma entre esses dois numeros é %i", soma);
+  return 0;
+}
